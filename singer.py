13d224abf12bd13dca24b0cc35665f3a3687fbda
David A. Samson <david.andrew.engineer@gmail.com>
2020-02-27 00:01:36 -0500

    added ability for singer to sing syllables with multiple phonemes

--- singer.py
+++ singer.py
@@ -20,7 +20,7 @@ class singer():
     def __init__(self, singer_name='matt', FS_out=192000, mode='lpc'):#FS_out=192000, mode='sample'):
         self.name = singer_name
         
-        self.FS = None
+        self.FS_in = None
         self.FS_out = FS_out
 
         assert(mode in ['sample', 'lpc'])
@@ -39,8 +39,10 @@ class singer():
         self.lpc = {} #coefficients and gains for lpc synthesis
         self.lpc_order = None
 
-        self.load_samples()
-        self.load_lpc()
+        if self.mode == 'sample': 
+            self.load_samples()
+        else:
+            self.load_lpc()
 
 
     def load_samples(self):
@@ -59,10 +61,10 @@ class singer():
                     upper = roots[-2] if roots[-2] - (length - 2) > 0 else roots[-1]
 
 
-                    if self.FS is None:     #set default sample rate, or verify that it's the same
-                        self.FS = rate
+                    if self.FS_in is None:     #set default sample rate, or verify that it's the same
+                        self.FS_in = rate
                     else:
-                        assert(rate == self.FS)
+                        assert(rate == self.FS_in)
 
                     #save the sample plus its crossover point to the object
                     self.phonemes.append(name)
@@ -97,12 +99,18 @@ class singer():
                     else:
                         assert(order == self.lpc_order)
 
+        #set up alias phonemes
+        for phoneme, alias in self.phoneme_letters['aliases'].items():
+            self.lpc[phoneme] = self.lpc[alias]
+            self.phonemes.append(phoneme)
+
         #silent/unpitched phoneme lpc parameters
         self.lpc['0'] = {
             'order': self.lpc_order,
             'coeffs': np.zeros(self.lpc_order, dtype=np.float64),
             'gain': np.zeros(1, dtype=np.float64),
         }
+
 
 
     def sing_excerpt(self, excerpt):
@@ -120,12 +128,16 @@ class singer():
         volumes = np.concatenate([np.ones(int(note['duration'] * self.FS_out), dtype=np.float64) * note['volume'] for note in excerpt])    
         pitch = np.concatenate([np.ones(int(note['duration'] * self.FS_out), dtype=np.float64) * (note['pitch'] if note['volume'] != 0 else 0.00000000001) for note in excerpt])
         
-        phonemes = np.concatenate([self.get_phoneme_sequence(note) for note in excerpt])
-        gains = np.concatenate([self.get_phoneme_gains(note) for note in excerpt])
+        phonemes, gains = zip(*[self.get_phoneme_gain_sequences(note) for note in excerpt])
+        phonemes = np.concatenate(phonemes)
+        gains = np.concatenate(gains)
+
+        # phonemes = np.concatenate([self.get_phoneme_sequence(note) for note in excerpt])
+        # gains = np.concatenate([self.get_phoneme_gains(note) for note in excerpt])
 
         #get buzz and noise. combine into a single source
         buzz, noise = pitched_squareDC(pitch, 0.25, sample_t), white_noise_t(sample_t)
-        source = 1.5 * buzz + 0.1 * noise
+        source = 1.5 * buzz + 0.3 * noise
 
         samples = np.zeros(num_samples + self.lpc_order, dtype=np.float64) #preallocate samples array. pad with 'order' 0s before the start of the sample output, so that the filter draws from them before we have generated 'order' samples 
 
@@ -143,39 +155,69 @@ class singer():
 
 
 
-    def get_phoneme_sequence(self, note):
+    # def get_phoneme_sequence(self, note):
+    #     """create an array of unicode characters representing the phoneme at each instant in the note"""
+    #     num_samples = int(note['duration'] * self.FS_out)
+    #     sequence = np.chararray(num_samples, unicode=True)
+
+    #     sequence[:] = '0' #set all unset phonemes to silent
+        
+    #     if note['volume'] != 0:
+            
+    #         count = 0
+    #         phoneme_duration_pairs = self.partition_syllable(note['syllable'], note['duration'])
+    #         for phoneme, duration in phoneme_duration_pairs:
+    #             if phoneme not in self.phonemes:
+    #                 phoneme = self.default_phoneme
+    #             phoneme_samples = int(note['duration'] * self.FS_out)
+    #             sequence[count:count+phoneme_samples] = phoneme
+    #             count += phoneme_samples
+    #         sequence[count:] = phoneme_duration_pairs[-1][0] #set any remaining samples to the last phoneme in the sequence
+
+    #     return sequence
+
+    # def get_phoneme_gains(self, note):
+    #     """create an array of gains for each phoneme at each instant in the note"""
+    #     num_samples = int(note['duration'] * self.FS_out)
+    #     gains = np.zeros(num_samples, dtype=np.float64)
+
+    #     if note['volume'] != 0:
+    #         assert(len(note['syllable']) == 1) #TODO->allow multiple phoneme syllable.
+    #         phoneme = note['syllable'] if note['syllable'] in self.phonemes else self.default_phoneme
+
+    #         #because not splitting up note into multiple phonemes at the moment, just assign the phoneme to the whole sample
+    #         gains[:] = self.lpc[phoneme]['gain']
+
+    #     return gains
+
+
+    def get_phoneme_gain_sequences(self, note):
         """create an array of unicode characters representing the phoneme at each instant in the note"""
         num_samples = int(note['duration'] * self.FS_out)
+
         sequence = np.chararray(num_samples, unicode=True)
-
         sequence[:] = '0' #set all unset phonemes to silent
-        
-        if note['volume'] != 0:
-            assert(len(note['syllable']) == 1) #TODO->allow multiple phoneme syllable.
-            phoneme = 'a' if note['syllable'] not in self.phonemes else note['syllable']
 
-            #because not splitting up note into multiple phonemes at the moment, just assign the phoneme to the whole sample
-            sequence[:] = phoneme
-
-        return sequence
-
-    def get_phoneme_gains(self, note):
-        """create an array of gains for each phoneme at each instant in the note"""
-        num_samples = int(note['duration'] * self.FS_out)
         gains = np.zeros(num_samples, dtype=np.float64)
+        
+        if note['volume'] != 0:            
+            count = 0 #current sample position in the arrays
+            for phoneme, duration in self.partition_syllable(note['syllable'], note['duration']):
+                if phoneme not in self.phonemes:
+                    phoneme = self.default_phoneme
+                phoneme_samples = int(duration * self.FS_out)
+                sequence[count:count+phoneme_samples] = phoneme
+                gains[count:count+phoneme_samples] = self.lpc[phoneme]['gain']
 
-        if note['volume'] != 0:
-            assert(len(note['syllable']) == 1) #TODO->allow multiple phoneme syllable.
-            phoneme = 'a' if note['syllable'] not in self.phonemes else note['syllable']
+                count += phoneme_samples
 
-            #because not splitting up note into multiple phonemes at the moment, just assign the phoneme to the whole sample
-            gains[:] = self.lpc[phoneme]['gain']
-
-        return gains
+            sequence[count:] = phoneme
+            gains[count:] = self.lpc[phoneme]['gain']
+        return sequence, gains
 
     # def get_phoneme_lpc_arrays(self, note):
     #     """create an array for the phoneme's lpc gains and coefficients"""
-    #     samples = int(note['duration'] * self.FS)
+    #     samples = int(note['duration'] * self.FS_in)
     #     coeffs = np.zeros((samples, self.lpc_order), dtype=np.float64)
     #     gains = np.zeros(samples, dtype=np.float64)
 
@@ -244,6 +286,8 @@ class singer():
 
     def partition_syllable(self, syllable, duration):
         #split the syllable into durations for each phoneme
+        #TODO: use rates from http://www.asel.udel.edu/icslp/cdrom/vol4/301/a301.pdf
+
         consonants = self.phoneme_letters['consonants']
         vowels = self.phoneme_letters['vowels']
 
@@ -299,15 +343,15 @@ def play(sample, FS=44100, block=True):
 
 if __name__ == '__main__':
     #demo of singing arpeggios of alleluia
-    FS_out = 192000
+    FS_out = 44100#192000
     
     matt = singer(singer_name='matt', FS_out=FS_out)
     
     pitches = [16.35, 17.32, 18.35, 19.45, 20.6, 21.83, 23.12, 24.5, 25.96, 27.5, 29.14, 30.87, 32.7, 34.65, 36.71, 38.89, 41.2, 43.65, 46.25, 49, 51.91, 55, 58.27, 61.74, 65.41, 69.3, 73.42, 77.78, 82.41, 87.31, 92.5, 98, 103.83, 110, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185, 196, 207.65, 220, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392, 415.3, 440, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.25, 698.46, 739.99, 783.99, 830.61, 880, 932.33, 987.77, 1046.5, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760, 1864.66, 1975.53, 2093, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520, 3729.31, 3951.07, 4186.01, 4434.92, 4698.63, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040, 7458.62, 7902.13]
     
     for pitch in pitches:
-        speed = 0.05
-        pitch = np.random.uniform(80, 400)
+        speed = 0.2
+        pitch = np.random.uniform(20, 260)
         print(pitch)
         notes = [
             {'syllable':'a', 'duration':1*speed, 'pitch':pitch*np.power(2,12/12), 'volume':1},
